00660481b9ec864bec196e4b31c7dd0de4e26c1b
sergio2pj <sergio6pj@gmail.com>
2022-04-27 14:56:52 -0300

    Usando dotenv em db.config

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,10 @@
+require('dotenv/config');
 module.exports = {
-    HOST: "",
-    USER: "",
-    PASSWORD: "",
-    DB: "",
-    dialect: "",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASS,
+    DB: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT,
     pool: {
       max: 5,
       min: 0,
