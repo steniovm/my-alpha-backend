f06d2a023164a7793a4f957076b26db1c6d4869c
sergio2pj <sergio6pj@gmail.com>
2022-04-27 16:55:13 -0300

    Adicionando o atributo PORT ao db.config

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,10 @@
-require('dotenv/config');
+require('dotenv').config()
 module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PASS,
     DB: process.env.DB_NAME,
+    PORT: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     pool: {
       max: 5,
