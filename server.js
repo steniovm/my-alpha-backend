0d1fb2da07a387e0e07f3576b02e606028759b85
sergio2pj <sergio6pj@gmail.com>
2022-04-27 14:27:46 -0300

    Adicionando uma rota básica ao server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.json({ message: "Bem-vindo ao aplicativo Alpha Edtech." });
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
