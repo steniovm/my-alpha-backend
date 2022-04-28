9d10a5a05f787ed4c17d40f3b36ce0d397fb8ff3
sergio2pj <sergio6pj@gmail.com>
2022-04-28 13:03:06 -0300

    Verificação de email, senha e data de aniversário

--- middleware/verifySignUP.js
+++ middleware/verifySignUP.js
@@ -7,12 +7,42 @@ checkDuplicateEmail = async (req, res, next) => {
   );
   if (response.rows.length > 0) {
     return res.status(400).send({
-      message: "Email already in use!"
+      message: "Email já cadastrado!"
+    });
+  }
+  next();
+};
+checkPassword = async (req, res, next) => {
+  const password = req.body.password;
+  if(password.length != 8 || !password.match(/\d/)){
+    return res.status(400).send({
+      message: "A senha deve ter 8 caracteres e conter pelo menos um número!"
+    });
+  }
+  next();
+};
+checkEmail = async (req, res, next) => {
+  const email = req.body.email;
+  if(!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)){
+    return res.status(400).send({
+      message: "Email inválido!"
+    });
+  }
+   next();
+};
+checkBirthday = async (req, res, next) => {
+  const birthday = req.body.birthday;
+  if(!birthday.match(/^\d{4}-\d{2}-\d{2}$/)){
+    return res.status(400).send({
+      message: "Data de nascimento inválida!"
     });
   }
   next();
 };
 const verifySignUp = {
-  checkDuplicateEmail: checkDuplicateEmail
+  checkDuplicateEmail: checkDuplicateEmail,
+  checkPassword: checkPassword,
+  checkEmail: checkEmail,
+  checkBirthday: checkBirthday
  };
 module.exports = verifySignUp;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 const { verifySignUp } = require("../middleware");
 const user = require("../controllers/profiler");
 const router = require("express-promise-router")();
-router.post("/signup", verifySignUp.checkDuplicateEmail, user.insert);
+router.post("/signup",  [verifySignUp.checkPassword, verifySignUp.checkBirthday, verifySignUp.checkEmail, verifySignUp.checkDuplicateEmail], user.insert);
 router.post("/signin", user.validate);
 module.exports = router;
