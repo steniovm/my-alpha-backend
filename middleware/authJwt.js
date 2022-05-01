cad399bc5bacf1b69d39d7422be2511e3fda8b01
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-04-30 22:33:25 -0300

    update: controle de sessao

--- middleware/authJwt.js
+++ middleware/authJwt.js
@@ -1,24 +1,56 @@
+const db = require("../config/db.config.js");
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
-verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+
+async function searchUUID (uuid)
+{
+  console.log(`Searching ${uuid}...`)
+  const {rows} = await db.query(
+    "SELECT * FROM users WHERE uuid = $1",
+    [uuid]
+  );
+  console.log(`Found ${rows.length}!`);
+  return rows.length > 0 ? rows[0] : null;
+}
+
+async function createToken (uuid, res) 
+{
+  const token = jwt.sign({uuid: uuid}, config.secret);
+  res.cookie('token', `${token}`, 
+    { 
+        maxAge: 900000, 
+        //path: "http://localhost/",
+        sameSite: "None",
+        secure: false 
+    });
+  return token;
+}
+
+async function verifyToken (req, res, next)
+{
+  //console.log(req.cookies)
+  const token = req.cookies ? req.cookies["token"] : null;//req.headers["x-access-token"];
+  //console.log(token)
   if (!token) {
     return res.status(403).send({
       message: "No token provided!"
     });
   }
-  jwt.verify(token, config.secret, (err, decoded) => {
+  jwt.verify(token, config.secret, async (err, decoded) => {
     if (err) {
       return res.status(401).send({
         message: "Unauthorized!"
       });
     }
-    req.userId = decoded.id;
+    const user = await searchUUID(decoded.uuid);
+    console.log(`Verified ${user.email}`);
+    req.user = user;
     next();
   });
 };
 
-const authJwt = {
-  verifyToken: verifyToken
+module.exports = {
+  verifyToken,
+  createToken,
+  searchUUID
 };
-module.exports = authJwt;
