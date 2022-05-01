7c1989b34dfcc111d9f615b6926c0332242dceb3
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-04-30 22:33:08 -0300

    update: cookie-parser

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require('cookie-parser');
 const authRoutes = require("./routes/auth.routes");
 const userRoutes = require("./routes/user.routes");
 const app = express();
+
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }))
